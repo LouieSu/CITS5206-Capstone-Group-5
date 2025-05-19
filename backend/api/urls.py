508b6bdfd28cf1259ade3241a693b37ae95f93b2
S.W.You <songwenyou@gmail.com>
2025-05-19 18:02:10 +0800

    UI: show a tiny suggestion box and integrate it with backend api. Removed legacy used API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import setup_signal, save_user_info, rules_set, units, default_plan,validate_mit2024,validate_mit2025,validate_mds2024, validate_mds2025
+from .views import setup_signal, rules_set, units, default_plan,validate_mit2024,validate_mit2025,validate_mds2024, validate_mds2025
 
 
 urlpatterns = [
     path('setup_signal/', setup_signal),  
-
-    path('api/save_user_info', save_user_info),
 
     #restfule APIs providing ruleset and units.
     path('ruleset/<str:ruleset_code>', view=rules_set, name="rule_set"),
